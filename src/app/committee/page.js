d136e912a46caa2f48aae231ea933f185ff2f080
Vishal Jangid <vishal.jangid_it22@pccoer.in>
2025-03-23 21:45:45 +0530

    Management Content Update

---
+++ src/app/committee/page.js
@@ -0,0 +1,109 @@
+import React from "react";
+
+const SteeringCommittee = () => {
+  return (
+    <div className="p-6 max-w-3xl mx-auto ">
+      <h2 className="text-2xl font-medium mb-4">Steering Committee</h2>
+
+      <h3 className="text-xl font-semibold mt-4">Chief Patrons</h3>
+      <ul className="list-disc list-inside text-gray-800 mb-4">
+        <li>
+          <span className="font-semibold italic text-blue-800">
+            Shri. Dnyaneshwar P. Landge
+          </span>
+          , Chairman, PCET
+        </li>
+        <li>
+          <span className="font-semibold italic text-blue-800">
+            Smt. Padma M. Bhosale
+          </span>
+          , Vice Chairperson, PCET
+        </li>
+        <li>
+          <span className="font-semibold italic text-blue-800">
+            Shri. Vitthal S. Kalbhor
+          </span>
+          , Secretary, PCET
+        </li>
+        <li>
+          <span className="font-semibold italic text-blue-800">
+            Shri. Shantaram D. Garade
+          </span>
+          , Treasurer, PCET
+        </li>
+        <li>
+          <span className="font-semibold italic text-blue-800">
+            Shri. Harshwardhan S. Patil
+          </span>
+          , Trustee, PCET
+        </li>
+        <li>
+          <span className="font-semibold italic text-blue-800">
+            Dr. Girish Desai
+          </span>
+          , Executive Director, PCET
+        </li>
+      </ul>
+
+      <h3 className="text-xl font-semibold">Patrons</h3>
+      <ul className="list-disc list-inside text-gray-800 mb-4">
+        <li>
+          <span className="font-semibold italic text-blue-800">
+            Dr. Harish Tiwari
+          </span>
+          , Principal
+        </li>
+      </ul>
+
+      <h3 className="text-xl font-semibold">General Chair</h3>
+      <ul className="list-disc list-inside text-gray-800 mb-4">
+        <li>
+          <span className="font-semibold italic text-blue-800">
+            Dr. Gulab D. Siraskar
+          </span>
+          , Head of the Department, Mechanical Engineering
+        </li>
+      </ul>
+
+      <h3 className="text-xl font-semibold">Co-ordinators</h3>
+      <ul className="list-disc list-inside text-gray-800 mb-4">
+        <li>
+          <span className="font-semibold italic text-blue-800">
+            Dr. Ramesh K, Rathod
+          </span>
+          , Associate Professor, Mechanical Engineering
+        </li>
+        <li>
+          <span className="font-semibold italic text-blue-800">
+            Prof.Dr. Rupali Patil
+          </span>
+          , Assistant Professor, Mechanical Engineering
+        </li>
+      </ul>
+
+      <h3 className="text-xl font-semibold">Co-coordinators</h3>
+      <ul className="list-disc list-inside text-gray-800 mb-4">
+        <li>
+          <span className="font-semibold italic text-blue-800">
+            Dr. Sukhadip M. Chougule
+          </span>
+          , Assistant Professor, Mechanical Engineering
+        </li>
+        <li>
+          <span className="font-semibold italic text-blue-800">
+            Dr. Ganesh Mahadev Fodase
+          </span>
+          , Assistant Professor, Mechanical Engineering
+        </li>
+        <li>
+          <span className="font-semibold italic text-blue-800">
+            Dr. Deepak D. Biradar
+          </span>
+          , Assistant Professor, Mechanical Engineering
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default SteeringCommittee;
---
+++ src/app/review/page.js
@@ -0,0 +1,117 @@
+import React from "react";
+
+const AdvisoryCommittee = () => {
+  return (
+    <div className="p-6 max-w-4xl mx-auto">
+      <h2 className="text-2xl font-medium mb-4">Advisory Committee</h2>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-gray-800 mt-2">
+        <p>
+          <span className="font-semibold italic text-blue-800">
+            Dr. Swaroop V. Darbha
+          </span>
+          , Professor, Department of Mechanical Engineering, Texas A&M
+          University, Texas
+        </p>
+        <p>
+          <span className="font-semibold italic text-blue-800">
+            Dr. Williams R. Calderon Munoz
+          </span>
+          , Department Chairman, Department of Mechanical Engineering,
+          Universidad de Chile, Chile
+        </p>
+        <p>
+          <span className="font-semibold italic text-blue-800">
+            Dr. Zafar Said
+          </span>
+          , Associate Professor, Department of Mechanical and Aerospace
+          Engineering, UAE University, UAE
+        </p>
+        <p>
+          <span className="font-semibold italic text-blue-800">
+            Dr. Kishor Purushottam Kolhe
+          </span>
+          , Professor, Faculty of Mechanical and Industrial Engineering, Woldia
+          Institute of Technology, Woldia University, Ethopia
+        </p>
+        <p>
+          <span className="font-semibold italic text-blue-800">
+            Dr. Sudarshankumar
+          </span>
+          , Professor & Head of IT, VIT, Pune, India
+        </p>
+        <p>
+          <span className="font-semibold italic text-blue-800">
+            Dr. Ajit M. Kate
+          </span>
+          , Principal, Zeal College of Engineering and Research, Pune
+        </p>
+        <p>
+          <span className="font-semibold italic text-blue-800">
+            Dr. M.J. Sable
+          </span>
+          , Professor , COEP Technological University ,Pune
+        </p>
+        <p>
+          <span className="font-semibold italic text-blue-800">
+            Dr. Pradeep A. Patil,
+          </span>
+          , Chairman, BOS ( Mech. Engg.) SPPU Pune and Professor and Head,
+          Mechanical Engineering, JSPM’s, Jayawantrao Sawant College of
+          Engineering , Pune
+        </p>
+        <p>
+          <span className="font-semibold italic text-blue-800">
+            Dr. Narendra R. Deore
+          </span>
+          , BOS ( Mech. Engg.) SPPU Pune and Professor, Pimpri Chinchwad
+          Engineering College, Pune
+        </p>
+        <p>
+          <span className="font-semibold italic text-blue-800">
+            Prof. Amit R. Patil
+          </span>
+          , Assistant Professor, Mechanical Department, M.E.S College of
+          Engineering, Pune
+        </p>
+        <p>
+          <span className="font-semibold italic text-blue-800">
+            Prof. Dr. Rahul M. Mapari
+          </span>
+          , Head of Department, Electronics and Telecommunication Engineering,
+          Pimpri Chinchwad College of Engineering and Research, Pune
+        </p>
+        <p>
+          <span className="font-semibold italic text-blue-800">
+            Prof. Dr. Archana A. Chaugule
+          </span>{" "}
+          , Head of Department, Computer Engineering, Pimpri Chinchwad College
+          of Engineering and Research, Pune
+        </p>
+        <p>
+          <span className="font-semibold italic text-blue-800">
+            Prof. Dr. Sudarshan S. Bobade
+          </span>
+          , Head of Department, Civil Engineering, Pimpri Chinchwad College of
+          Engineering and Research, Pune
+        </p>
+        <p>
+          <span className="font-semibold italic text-blue-800">
+            Prof. Dr. Santosh V. Chobe
+          </span>
+          , Head of Department, Information Technology Engineering, Pimpri
+          Chinchwad College of Engineering and Research, Pune
+        </p>
+        <p>
+          <span className="font-semibold italic text-blue-800">
+            Prof. Tushar B. Gaikwad
+          </span>
+          , Head of Department, First Year Engineering, Pimpri Chinchwad College
+          of Engineering and Research, Pune
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AdvisoryCommittee;
